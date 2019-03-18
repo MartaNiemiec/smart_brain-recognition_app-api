773338405e8decd5ec8eac95b014168de3480430
Marta Niemiec <niemiec.marta@hotmail.com>
2019-03-18 13:55:45 +0100

    adding database connection

--- server.js
+++ server.js
@@ -15,10 +15,8 @@ const db = knex({
   client: 'pg',
   connection: {
     // host : '127.0.0.1', //127.0.0.1 = localhost
-    host: 'postgresql-infinite-57163',
-    user : 'marta',
-    password : '',
-    database : 'smart-brain'
+    host: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
